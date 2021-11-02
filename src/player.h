f0e92dd3400e768958aa7d1d3f4da7493ce013cf
raniadf <16520006@mahasiswa.itb.ac.id>
2021-11-02 10:46:27 +0700

    Add some feature

--- src/player.h
+++ src/player.h
@@ -7,11 +7,18 @@
 typedef struct {
    ListPos gadget; /* untuk inventory gadget */
    int money; /* menyimpan uang player */
+   int time; /* menyimpan waktu player */
    // NANTI TAMBAHIN YANG LAIN JUGA!!
 } Player;
 
 #define GADGET(p) (p).gadget
 #define MONEY(p) (p).money
+#define TIME(p) (p).time
+
+/* Inisialisasi Player */
+void createPlayer (Player *p);
+/* I.S. Nama player terdefinisi */
+/* F.S. Membuat player dengan atribut gadget, money, time, dan ... */
 
 /* *** Primitif Uang *** */
 /* Menambah uang */
@@ -23,5 +30,10 @@ void addMoney (Player *p, int val);
 void useMoney (Player *p, int val);
 /* I.S. Player dan val sudah terdefinisi */
 /* F.S. Uang player bertambah sejumlah val */
+
+/* *** Primitif Waktu *** */
+void changeTime (Player *p, int val);
+/* I.S. Player dan val sudah terdefinisi */
+/* F.S. Waktu player bertambah/berkurang sejumlah val */
 
 #endif
