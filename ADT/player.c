a03fc59352842abd81c3f6c46d583b28534f88ab
raniadf <16520006@mahasiswa.itb.ac.id>
2021-11-18 18:45:52 +0700

    change folder player

---
+++ ADT/player.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "player.h"
+
+/* *** Kreator *** */
+void createPlayer (Player *p){
+/* I.S. sembarang */
+/* F.S. Membuat player dengan atribut sebagai berikut : */
+/*      Gadget = Listpos kosong */
+/*      Uang = 0 yen */
+/*      Waktu = 0 satuan */
+    
+    // Membentuk penyimpanan sementara untuk listpos gadget
+    CreateListPos(&GADGET(*p));
+    // Assign data ke masing-masing atribut
+    MONEY(*p) = 0;
+    TIME(*p) = 0;
+    POSISI(*p) = MakePOINT('8', 0, 0);
+}
+
+/* *** Primitif Uang *** */
+/* Menambah uang */
+void addMoney (Player *p, int val){
+/* I.S. Player dan val sudah terdefinisi */
+/* F.S. Uang player bertambah sejumlah val */
+    MONEY(*p) += val;
+}
+
+/* Menggunakan Uang */
+void useMoney (Player *p, int val){
+/* I.S. Player dan val sudah terdefinisi */
+/* F.S. Uang player bertambah sejumlah val */
+    MONEY(*p) -= val;
+}
+
+/* *** Primitif Waktu *** */
+void changeTime (Player *p, int val){
+/* I.S. Player dan val sudah terdefinisi */
+/* F.S. Waktu player bertambah/berkurang(apabila val berupa bilangan negatif) sejumlah val */
+    TIME(*p) += val;
+}
---
+++ ADT/player.h
@@ -0,0 +1,47 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "../ADT/boolean.h"
+#include "../ADT/listpos.h"
+#include "../ADT/point.h"
+
+/* Definisi elemen */
+typedef struct {
+   ListPos gadget; /* untuk inventory gadget */
+   int money; /* menyimpan uang player */
+   int time; /* menyimpan waktu player */
+   POINT location; /* menyimpan posisi pemain */
+} Player;
+
+/* ********* AKSES (Selektor) ********* */
+/* Jika p adalah Player, maka akses elemen : */
+#define GADGET(p) (p).gadget
+#define MONEY(p) (p).money
+#define TIME(p) (p).time
+#define POSISI(P) (P).location
+
+/* *** Kreator *** */
+void createPlayer (Player *p);
+/* I.S. sembarang */
+/* F.S. Membuat player dengan atribut sebagai berikut : */
+/*      Gadget = Listpos kosong */
+/*      Uang = 0 yen */
+/*      Waktu = 0 satuan */
+
+/* *** Primitif Uang *** */
+/* Menambah uang */
+void addMoney (Player *p, int val);
+/* I.S. Player dan val sudah terdefinisi */
+/* F.S. Uang player bertambah sejumlah val */
+
+/* Menggunakan Uang */
+void useMoney (Player *p, int val);
+/* I.S. Player dan val sudah terdefinisi */
+/* F.S. Uang player bertambah sejumlah val */
+
+/* *** Primitif Waktu *** */
+void changeTime (Player *p, int val);
+/* I.S. Player dan val sudah terdefinisi */
+/* F.S. Waktu player bertambah/berkurang sejumlah val */
+
+#endif
